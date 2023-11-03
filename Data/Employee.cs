cfcfa9d895ec7a0c30bd3f5f55e52a62c7392bad
Jannick Mørup <jannickmorup@gmail.com>
2023-11-03 10:47:24 +0100

    Add project files.

---
+++ Data/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeCrudBlazor.Data
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string PostalCode { get; set; }
+        public string Address { get; set; }
+        public string EmailAdress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string city { get; set; }
+        public string Designation { get; set; }
+    }
+}
